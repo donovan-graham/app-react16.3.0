a94940265432428ad1c5bde05af2a06e22142c37
Donovan Graham <donovan@platform7.com>
2018-03-08 21:56:31 +0200

    fix: add date-fns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import parse from 'date-fns/parse';
+import format from 'date-fns/format';
+import isToday from 'date-fns/is_today';
+// import {parse, format, isToday} from 'date-fns/esm' <-- upcoming release v2.0.0
+
 import { Theme } from './Context';
 import Child from './Child';
 
@@ -16,9 +21,15 @@ class App extends React.Component {
   }
 
   render() {
+    const date = '2018-03-08T00:30:30' || new Date();
+    const dateStr = format(date, 'DD MMM YYYY');
+    const todayStr = isToday(date) ? 'Today' : 'Not today';
+
     return (
       <Theme.Provider value={this.state.theme}>
-        <span>App</span>
+        <span>
+          {dateStr} ? {todayStr}
+        </span>
         <br />
         <button onClick={this.onClick}>Change Theme</button>
         <div id="parent">
